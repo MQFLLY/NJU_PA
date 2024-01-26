4266b3b55a8915c7326085235644f1155cf0666f
tracer-ics2023 <tracer@njuics.org>
2024-01-26 10:45:39 +0800

    >  compile NEMU 202314021003 Mao Linux mqf-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:45:39 up 2 days, 17:42,  1 user,  load average: 0.38, 0.27, 0.24

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -109,7 +109,7 @@ static bool make_token(char *e) {
 
         switch (rules[i].token_type) {
           case TK_NOTYPE: break;
-          default: tokens[nr_token++].type = rules[i].token_type, strncpy(tokens[nr_token++].str,substr_start,substr_len);
+          default: tokens[nr_token].type = rules[i].token_type, strncpy(tokens[nr_token++].str,substr_start,substr_len);
           break;
         }
 
